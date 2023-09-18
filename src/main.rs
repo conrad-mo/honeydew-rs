2a965688f45b581dba40674d0feb77b612319b65
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-18 19:03:31 -0400

    Initial backend commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+mod types;
+
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
+use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
+use crate::types::CVLetter;
+
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::new().allow_origin(Any);
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/generate", get(newcv))
+        .layer(cors);
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "404"
+}
+async fn newcv() -> impl IntoResponse {
+    let letter = new CVLetter();
+    (StatusCode::OK, Json(letter))
+}
---
+++ src/types.rs
@@ -0,0 +1,11 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct CVLetter {
+    pub date: String,
+    pub firstparagraph: String,
+    pub experienceparagraphone: String,
+    pub experienceparagraphtwo: String,
+    pub endingparagraph: String,
+    pub name: String,
+}
