f6c8520ce78851b2dffd83e99518185c0490b2b9
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-18 22:22:13 -0400

    Response works but deserializing doesnt give answer

--- src/types.rs
+++ src/types.rs
@@ -1,13 +1,9 @@
-use reqwest::header::HeaderMap;
-use serde::Serialize;
-use serde::Deserialize;
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::{Error, Response};
+use serde::{Serialize, Deserialize};
+use serde_json::Result as SerdeJsonResult;
 
 use crate::api;
-#[derive(Debug, Deserialize)]
-pub struct APIResponse {
-    message: String,
-    error: String,
-}
 #[derive(Serialize)]
 pub struct CVLetter {
     pub date: String,
@@ -18,16 +14,41 @@ pub struct CVLetter {
     pub name: String,
 }
 
+#[derive(Serialize)]
+pub struct APIData {
+    model: String,
+    prompt: String,
+    max_tokens: i32,
+    temperature: f64,
+    k: i32,
+    stop_sequences: Vec<String>,
+    return_likelihoods: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct APIResponse {
+    message: String,
+    error: String,
+}
+
 impl CVLetter {
-    pub async fn generate_paragraph1() -> Result<APIResponse, reqwest::Error> {
+    pub async fn generate_paragraph1(){
         let client = reqwest::Client::new();
+        let request_data = APIData {
+            model: "command".to_string(),
+            prompt: "Write a body paragraph about why someone should apply to RBC".to_string(),
+            max_tokens: 300,
+            temperature: 0.9,
+            k: 0,
+            stop_sequences: Vec::new(),
+            return_likelihoods: "NONE".to_string(),
+        };
         let mut headers = HeaderMap::new();
-        headers.insert("content-type", "application/json".parse().unwrap());
-        headers.insert("Authorization", format!("Bearer {}", api::APIKEY).parse().unwrap());
-        let resp = client.post("https://api.cohere.ai/v1/generate").headers(headers).send().await;
-        let resp_json = resp?.json::<APIResponse>().await;
-        println!("{:?}", resp_json);
-        resp_json
+        headers.insert("Authorization", HeaderValue::from_str(&format!("BEARER {}", api::APIKEY)).unwrap());
+        headers.insert("Content-Type", HeaderValue::from_static("application/json"));
+        let response = client.post("https://api.cohere.ai/v1/generate").headers(headers).json(&request_data).send().await;
+        let pog = response.unwrap();
+        println!("{:?}", pog);
     }
     async fn generate_experienceparagraph1() -> String {
         String::from("Yes")
