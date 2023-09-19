699180b7c6db4115396eafed5ac92af847f92138
conrad-mo <51024076+conrad-mo@users.noreply.github.com>
2023-09-18 20:53:46 -0400

    Broken stuff for now

--- src/api.rs
+++
@@ -1,1 +0,0 @@
-pub const APIKEY: &str = "PUT COHERE API KEY HERE";
--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,13 @@
+use reqwest::header::HeaderMap;
 use serde::Serialize;
+use serde::Deserialize;
 
+use crate::api;
+#[derive(Debug, Deserialize)]
+pub struct APIResponse {
+    message: String,
+    error: String,
+}
 #[derive(Serialize)]
 pub struct CVLetter {
     pub date: String,
@@ -8,4 +16,26 @@ pub struct CVLetter {
     pub experienceparagraphtwo: String,
     pub endingparagraph: String,
     pub name: String,
+}
+
+impl CVLetter {
+    async fn generate_paragraph1() -> Result<APIResponse, reqwest::Error> {
+        let client = reqwest::Client::new();
+        let mut headers = HeaderMap::new();
+        headers.insert("content-type", "application/json".parse().unwrap());
+        headers.insert("Authorization", format!("Bearer {}", api::APIKEY).parse().unwrap());
+        let resp = client.post("https://api.cohere.ai/v1/generate").headers(headers).send().await;
+        let resp_json = resp?.json::<APIResponse>().await;
+        println!("{:?}", resp_json);
+        resp_json
+    }
+    async fn generate_experienceparagraph1() -> String {
+        String::from("Yes")
+    }
+    async fn generate_experienceparagraph2() -> String {
+        String::from("Yes")
+    }
+    async fn generate_endingparagraph() -> String {
+        String::from("Yes")
+    }
 }
